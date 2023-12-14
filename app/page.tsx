c0d17ea94f4f01a496072ccbabbe3456846897d6
lokesh <lokeshkhati005@gmail.com>
2023-12-14 10:30:41 +0530

    initial commit -m

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/Container";
+import TicTacToe from "@/questions/tic-tac-toe/TicTacToe";
+
+export default function Home() {
+  return (
+    <Container>
+      <TicTacToe />
+    </Container>
+  )
+}
---
+++ questions/tic-tac-toe/TicTacToe.tsx
@@ -0,0 +1,18 @@
+import Heading from "@/components/Heading"
+
+const TicTacToe = () => {
+    return (
+        <div className="border border-gray-900 w-full h-full">
+            <Heading>TicTacToe</Heading>
+            <p></p>
+            <div>
+                {
+
+                }
+            </div>
+
+            <button></button>
+        </div>
+    )
+}
+export default TicTacToe
